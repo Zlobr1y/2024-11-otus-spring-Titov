80c1f34f06c9e7ecebe9aaa7aac41d8687fe929e
ATitov <Atitov@x5.ru>
2025-03-18 23:22:42 +0300

    Revert "hw03"
    This reverts commit 792d24e92dbedbb40c707222a1b9922750c30de8.

--- hw03/src/main/java/ru/otus/hw/config/TestConfig.java
+++
@@ -1,8 +0,0 @@
-package ru.otus.hw.config;
-
-public interface TestConfig {
-
-    int getRightAnswersCountToPass();
-
-    int getQuestionCount();
-}
--- hw03/src/main/java/ru/otus/hw/dao/QuestionDao.java
+++
@@ -1,9 +0,0 @@
-package ru.otus.hw.dao;
-
-import java.util.List;
-import ru.otus.hw.domain.Question;
-
-public interface QuestionDao {
-
-    List<Question> findAll();
-}
